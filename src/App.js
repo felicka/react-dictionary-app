dc6ef6e9d38028c86d4cc95fb83f95d9ed70744d
felicka <104525905+felicka@users.noreply.github.com>
2022-10-12 09:03:24 -0300

    created a search engine and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from "./felicia-logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
-      <h1>Dictionary</h1>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          React Dictionary App Coded by Felicia on{" "}
+          <a
+            href="https://github.com/felicka/react-dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for the definition of ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <h1>Dictionary</h1>
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} autofocus={true} />
+      </form>
+    </div>
+  );
+}
